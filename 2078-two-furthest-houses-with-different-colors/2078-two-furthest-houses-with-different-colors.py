d9f9396dbe39af4ced33f151d026365907292dc6
sasidhar7569 <sasidharmusalaboina@gmail.com>
2026-05-06 20:51:18 +0530

    Time: 0 ms (100%), Space: 19.4 MB (19.54%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -3,13 +3,13 @@ class Solution:
         ans=0
         k=len(colors)
         for i in range(k):
+           
+            if colors[i]!=colors[0]:
+                ans=max(ans,i)
             if colors[i]!=colors[k-1]:
-                x=abs((i-(k-1)))
-                ans=max(ans,x)
-        for j in range(k-1,0,-1):
-            if colors[j]!=colors[0]:
-                x=abs(j)
-                ans=max(ans,x)
+                x=abs(i-(k-1))
+                ans=max(ans,x)    
 
+        
                         
         return  ans          
