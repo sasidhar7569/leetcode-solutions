adba7fa51ab64013ce2e29a863a7a87112ffd53f
sasidhar7569 <sasidharmusalaboina@gmail.com>
2026-06-02 08:51:03 +0530

    Time: 0 ms (100%), Space: 19.1 MB (87.83%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,7 +1,19 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        ans=0
+        dici={}
         for ch in stones:
-            if ch in jewels:
-                ans+=1
-        return ans        
+            if ch in dici:
+                dici[ch]+=1
+            else:
+                dici[ch]=1
+        ans=0        
+        for j in jewels:
+            if j in dici:
+                ans=ans+dici[j]
+        return ans 
+        # ans=0
+        # for ch in stones:
+        #     if ch in jewels:
+        #         ans+=1
+        # return ans           
+
