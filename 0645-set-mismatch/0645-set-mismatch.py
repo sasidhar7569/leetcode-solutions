f4be84c7d5cf8489b6d5ad64becdb7a5c64b8064
sasidhar7569 <sasidharmusalaboina@gmail.com>
2026-06-14 21:00:29 +0530

    Time: 3039 ms (12.88%), Space: 20.7 MB (54.54%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        s=[]
+        k=len(nums)
+        dici={}
+        for j in nums:
+            if j not in dici:
+                dici[j]=1
+            else:
+                dici[j]+=1
+        for j in dici:
+            if dici[j]==2:
+                s.append(j)        
+        for i in range(1,k+1):
+            if i in nums:
+                continue
+            else:
+                s.append(i)        
+                  
+        
+        return s          
+        
